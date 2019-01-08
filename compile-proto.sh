dcd31c84ddb1ede62df079c5abdcecfe704cdf2f
Andrey Vokhmin <avokhmin@spotware.com>
2019-01-08 17:35:59 +0200

    Demo version of the xServer communication

---
+++ compile-proto.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export SRC_DIR=../proto/
+export DST_DIR=./
+
+protoc -I=$SRC_DIR --python_out=$DST_DIR \
+    $SRC_DIR/CommonMessages_External.proto \
+    $SRC_DIR/CommonModelMessages_External.proto \
+    $SRC_DIR/XSMessages_External.proto \
+    $SRC_DIR/XSModelMessages_External.proto
